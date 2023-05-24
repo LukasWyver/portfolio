993bce086f79bbda84bae61488db015b63fd6ec1
LukasWyver <lucas_sin5@hotmail.com>
2023-05-24 13:57:19 -0300

    fonts config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+      },
+
+      colors: {
+        background: '#121214',
+        text: '#e1e1e5',
+        primary: '#8257e5',
       },
     },
   },
